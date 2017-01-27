156b6e41bd9014c32df37cdb8dd7fda3c03611b3
Keenen Gilles Harrisson <kenydbzgt@gmail.com>
2017-01-27 09:50:27 +0100

    chnage name and switch socket

--- webserver/main.c
+++ webserver/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 int main(int argc, char **argv)
-{ /* Arnold Robbins in the LJ of February ’95, describing RCS */
-  if (argc > 1 && strcmp(argv[1], "-advice") == 0)
-  { 
-     printf("Don’t Panic!\n");
-   return 42;
-  }
-   printf("Need an advice?\n");
+{
+  int socket_serveur;
+  socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
+   if (socket_serveur == -1)
+   {
+     /* traitement de l’erreur */
+     perror("socket_serveur");
+   } /* Utilisation de la socket serveur */
+   
    return 0;
  }
