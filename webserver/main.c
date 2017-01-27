3ebe0f84e1c9df7276e8b3c95d6b98b4a98dfb62
Keenen Gilles Harrisson <kenydbzgt@gmail.com>
2017-01-27 11:38:34 +0100

    socket IPv4
    nc 127.0.0.1 8080

--- webserver/main.c
+++ webserver/main.c
@@ -1,14 +1,37 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 #include <string.h>
-int main(int argc, char **argv)
+#include <sys/types.h>
+#include "socket.h"
+
+int main(void/*int argc, char **argv*/)
 {
-  int socket_serveur;
-  socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
-   if (socket_serveur == -1)
-   {
-     /* traitement de l’erreur */
-     perror("socket_serveur");
-   } /* Utilisation de la socket serveur */
-   
-   return 0;
+  int socket_srv;
+  int socket_client;
+  char recvBuff[1024];
+  int n;
+
+  n = 0;
+  socket_srv = creer_serveur(8080);
+  socket_client = accept(socket_srv, NULL, NULL);
+
+  if (socket_client == -1)
+  {
+    perror("accept error");
+   }
+  while ((n = read(socket_client, recvBuff, sizeof(recvBuff)-1)) > 0)
+  {
+    recvBuff[n] = 0;
+    if(fputs(recvBuff, stdout) == EOF)
+    {
+        printf("nope");
+    }
+  }
+
+  return 0;
  }
---
+++ webserver/socket.c
@@ -0,0 +1,40 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+
+int creer_serveur(int port)
+{
+  int socketfd;
+  struct sockaddr_in srv_addr;
+
+  srv_addr.sin_family = AF_INET;
+  srv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  srv_addr.sin_port = htons(port);
+
+  socketfd = socket(AF_INET, SOCK_STREAM, 0);
+
+   if (socketfd == -1)
+   {
+     perror("socket error");
+     return -1;
+   }
+
+   if(bind(socketfd, (struct sockaddr *)&srv_addr, sizeof(srv_addr)) == -1)
+   {
+     perror("bind socket error");
+     return -1;
+   }
+
+   if(listen(socketfd, 10) == -1)
+   {
+     perror("bind socket error");
+     return -1;
+   }
+   return socketfd;
+}
