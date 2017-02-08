fd09790aa32067bf882b7f37af330d8c94ebb434
Keenen Gilles Harrisson <kenydbzgt@gmail.com>
2017-02-08 15:00:54 +0100

    add REUSEADDR opt + wait for son

--- webserver/main.c
+++ webserver/main.c
@@ -12,20 +12,20 @@
 int main(void/*int argc, char **argv*/)
 {
   int socket_srv4;
-  //int socket_srv6;
+  int socket_srv6;
   int socket_client4;
-  //int socket_client6;
-  char recvBuff[1024];
+  int socket_client6;
   char welcome[256] = "Ohayo\nTire au Lapin\nChasseur chassant chausette\nWill Crappy creep\nFreddy les griffe du night\ndeja a cours d'idee\nunicode plz (づ◔ ͜ʖ◔)づ\nsocketv6 marche steup ( ﾟロ ﾟ)\nla magie du gwak (∩ ͡°ᴥ ͡°)⊃━☆ﾟ.*\nNOTHING¯\\_ツ_/¯\n\0";
 
   int pid4;
-  //int pid6;
+  int pid6;
   int n;
   n = 0;
   pid4 = fork();
   printf("fils4:%d\n", pid4);
   if (pid4 == 0)
   {
+    char recvBuff[1024];
     socket_srv4 = create_socket4(8080);
     if (socket_srv4 == -1)
     {
@@ -40,7 +40,7 @@ int main(void/*int argc, char **argv*/)
     }
 
     sleep(1);
-    
+
     if (write(socket_client4, welcome, strlen(welcome)) == -1)
     {
       perror("write4_welcome");
@@ -56,17 +56,18 @@ int main(void/*int argc, char **argv*/)
       if(fputs(recvBuff, stdout) == EOF)
       {
           printf("nope4");
-          //exit(0);
+          exit(0);
       }
     }
   }
 
-/*
+
   pid6 = fork();
   printf("fils6:%d\n", pid6);
   if (pid6 == 0)
   {
-    socket_srv6 = create_socket6(8081);
+    char recvBuff[1024];
+    socket_srv6 = create_socket6(8080);
     if (socket_srv6 == -1)
     {
       perror("create socket6 error");
@@ -92,7 +93,8 @@ int main(void/*int argc, char **argv*/)
       }
     }
   }
-*/
+wait(NULL);
+wait(NULL)
 
 
 
